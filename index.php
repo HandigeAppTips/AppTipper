c1d01e33ac5f31de7ec8aae7044829f4e0804795
StefBrusselers <stefbrusselers@handigeapptips.nl>
2015-12-07 09:57:42 +0100

    Create index.php
    index.php gemaakt

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Index</title>
+  </head>
+  <body>
+    <h1>Titel</h1>
+    
+    <p>Dit is wat tekst.</p>
+    
+    <!-- laat het php-script draaien -->
+    <?php
+      include "src/loginToDatabase.php";
+    ?>
+
+  </body>
+</html>
